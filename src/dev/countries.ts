01fed8d7fafc7289a13978a495e0ad56188080db
FlavioLionelRita <flaviolrital@hotmail.com>
2024-03-01 02:27:20 +0100

    feat: complete obj type to entities
    #0

--- src/dev/countries.ts
+++ src/dev/countries.ts
@@ -4,145 +4,112 @@ import { Type } from 'typ3s'
 import { SchemaFacadeBuilder } from '../lib'
 
 (async () => {
-	// const data = [
-	// {
-	// name: 'Spain',
-	// region: { name: 'Europe', code: 'EU' },
-	// languages: [{ name: 'Spanish' }, { name: 'Catalan' }, { name: 'Galician' }, { name: 'Basque' }],
-	// priority: 1,
-	// phoneCode: 34,
-	// timezones: [
-	// { name: 'Madrid', offset: 1, pos: { lat: 40.4165, log: -3.70256 } },
-	// { name: 'Ceuta', pos: { lat: 35.8883, log: -5.3162 } },
-	// { name: 'Canary', offset: 0, pos: { lat: 28.1248, log: -15.43 } }
-	// ]
-	// },
-	// {
-	// name: 'France',
-	// region: { name: 'Europe', code: 'EU' },
-	// languages: [{ name: 'French' }],
-	// priority: 1,
-	// phoneCode: 33,
-	// timezones: [
-	// { name: 'Paris', offset: 1, pos: { lat: 48.8566, log: 2.3522 } },
-	// { name: 'Reunion', offset: 4, pos: { lat: -20.8823, log: 55.4504 } },
-	// { name: 'Guadeloupe', offset: -4, pos: { lat: 16.265, log: -61.551 } }
-	// ]
-	// },
-	// {
-	// name: 'Germany',
-	// languages: [{ name: 'German' }],
-	// region: { name: 'Europe', code: 'EU' },
-	// priority: 1,
-	// phoneCode: 49,
-	// timezones: [
-	// { name: 'Berlin', offset: 1, pos: { lat: 52.5200, log: 13.4050 } },
-	// { name: 'Busingen', offset: 1, pos: { lat: 47.6963, log: 8.6927 } }
-	// ]
-	// },
-	// {
-	// name: 'Portugal',
-	// languages: [{ name: 'Portuguese' }],
-	// region: { name: 'Europe', code: 'EU' },
-	// priority: 1,
-	// phoneCode: 351,
-	// timezones: [
-	// { name: 'Lisbon', offset: 0, pos: { lat: 38.7223, log: -9.1393 } },
-	// { name: 'Madeira', offset: 0, pos: { lat: 32.7607, log: -16.9595 } },
-	// { name: 'Azores', offset: -1, pos: { lat: 37.7412, log: -25.6756 } }
-	// ]
-	// },
-	// {
-	// name: 'United States',
-	// languages: [{ name: 'English' }],
-	// region: { name: 'North America', code: 'NA' },
-	// priority: 1,
-	// phoneCode: 1,
-	// timezones: [
-	// { name: 'New York', offset: -5, pos: { lat: 40.7128, log: -74.0060 } },
-	// { name: 'Los Angeles', offset: -8, pos: { lat: 34.0522, log: -118.2437 } },
-	// { name: 'Chicago', offset: -6, pos: { lat: 41.8781, log: -87.6298 } },
-	// { name: 'Denver', offset: -7, pos: { lat: 39.7392, log: -104.9903 } },
-	// { name: 'Anchorage', offset: -9, pos: { lat: 61.2181, log: -149.9003 } },
-	// { name: 'Honolulu', offset: -10, pos: { lat: 21.3069, log: -157.8583 } }
-	// ]
-	// },
-	// {
-	// name: 'Canada',
-	// languages: [{ name: 'English' }, { name: 'French' }],
-	// region: { name: 'North America', code: 'NA' },
-	// priority: 1,
-	// phoneCode: 2,
-	// timezones: [
-	// { name: 'Toronto', offset: -5, pos: { lat: 43.6532, log: -79.3832 } },
-	// { name: 'Vancouver', offset: -8, pos: { lat: 49.2827, log: -123.1207 } },
-	// { name: 'Edmonton', offset: -7, pos: { lat: 53.5461, log: -113.4938 } },
-	// { name: 'Winnipeg', offset: -6, pos: { lat: 49.8951, log: -97.1384 } },
-	// { name: 'Yellowknife', offset: -7, pos: { lat: 62.4540, log: -114.3718 } },
-	// { name: 'Iqaluit', offset: -5, pos: { lat: 63.7467, log: -68.5170 } }
-	// ]
-	// },
-	// {
-	// name: 'Brasil',
-	// languages: [{ name: 'Portuguese' }],
-	// region: { name: 'South America', code: 'SA' },
-	// priority: 1,
-	// phoneCode: 55,
-	// timezones: [
-	// { name: 'Sao Paulo', offset: -3, pos: { lat: -23.5505, log: -46.6333 } },
-	// { name: 'Rio de Janeiro', offset: -3, pos: { lat: -22.9068, log: -43.1729 } },
-	// { name: 'Manaus', offset: -4, pos: { lat: -3.1190, log: -60.0217 } },
-	// { name: 'Porto Alegre', offset: -3, pos: { lat: -30.0346, log: -51.2177 } },
-	// { name: 'Fortaleza', offset: -3, pos: { lat: -3.7319, log: -38.5267 } },
-	// { name: 'Belem', offset: -3, pos: { lat: -1.4558, log: -48.4902 } }
-	// ]
-	// },
-	// {
-	// name: 'Argentina',
-	// languages: [{ name: 'Spanish' }],
-	// region: { name: 'South America', code: 'SA' },
-	// priority: 1,
-	// phoneCode: 54,
-	// timezones: [{ name: 'Buenos Aires', offset: -3, pos: { lat: -34.6037, log: -58.3816 } }]
-	// }
-	// ]
-
-	const dataTemp = [
+	const data = [
 		{
 			name: 'Spain',
-			region: { name: 'Europe', code: 'EU' }
+			region: { name: 'Europe', code: 'EU' },
+			languages: [{ code: 'SP', name: 'Spanish' }, { code: 'CAT', name: 'Catalan' }, { code: 'GAL', name: 'Galician' }, { code: 'BSQ', name: 'Basque' }],
+			timezones: [
+				{ name: 'Madrid', offset: 1, pos: { lat: 40.4165, log: -3.70256 } },
+				{ name: 'Ceuta', pos: { lat: 35.8883, log: -5.3162 } },
+				{ name: 'Canary', offset: 0, pos: { lat: 28.1248, log: -15.43 } }
+			],
+			priority: 1,
+			phoneCode: 34
 		},
 		{
 			name: 'France',
-			region: { name: 'Europe', code: 'EU' }
+			region: { name: 'Europe', code: 'EU' },
+			languages: [{ code: 'FR', name: 'French' }],
+			timezones: [
+				{ name: 'Paris', offset: 1, pos: { lat: 48.8566, log: 2.3522 } },
+				{ name: 'Reunion', offset: 4, pos: { lat: -20.8823, log: 55.4504 } },
+				{ name: 'Guadeloupe', offset: -4, pos: { lat: 16.265, log: -61.551 } }
+			],
+			priority: 1,
+			phoneCode: 33
 		},
 		{
 			name: 'Germany',
-			region: { name: 'Europe', code: 'EU' }
+			region: { name: 'Europe', code: 'EU' },
+			languages: [{ code: 'GER', name: 'German' }],
+			timezones: [
+				{ name: 'Berlin', offset: 1, pos: { lat: 52.5200, log: 13.4050 } },
+				{ name: 'Busingen', offset: 1, pos: { lat: 47.6963, log: 8.6927 } }
+			],
+			priority: 1,
+			phoneCode: 49
 		},
 		{
 			name: 'Portugal',
-			region: { name: 'Europe', code: 'EU' }
+			region: { name: 'Europe', code: 'EU' },
+			languages: [{ code: 'PT', name: 'Portuguese' }],
+			timezones: [
+				{ name: 'Lisbon', offset: 0, pos: { lat: 38.7223, log: -9.1393 } },
+				{ name: 'Madeira', offset: 0, pos: { lat: 32.7607, log: -16.9595 } },
+				{ name: 'Azores', offset: -1, pos: { lat: 37.7412, log: -25.6756 } }
+			],
+			priority: 1,
+			phoneCode: 351
 		},
 		{
 			name: 'United States',
-			region: { name: 'North America', code: 'NA' }
+			region: { name: 'North America', code: 'NA' },
+			languages: [{ code: 'EN', name: 'English' }],
+			timezones: [
+				{ name: 'New York', offset: -5, pos: { lat: 40.7128, log: -74.0060 } },
+				{ name: 'Los Angeles', offset: -8, pos: { lat: 34.0522, log: -118.2437 } },
+				{ name: 'Chicago', offset: -6, pos: { lat: 41.8781, log: -87.6298 } },
+				{ name: 'Denver', offset: -7, pos: { lat: 39.7392, log: -104.9903 } },
+				{ name: 'Anchorage', offset: -9, pos: { lat: 61.2181, log: -149.9003 } },
+				{ name: 'Honolulu', offset: -10, pos: { lat: 21.3069, log: -157.8583 } }
+			],
+			priority: 1,
+			phoneCode: 1
 		},
 		{
 			name: 'Canada',
-			region: { name: 'North America', code: 'NA' }
+			region: { name: 'North America', code: 'NA' },
+			languages: [{ code: 'EN', name: 'English' }, { code: 'FR', name: 'French' }],
+			timezones: [
+				{ name: 'Toronto', offset: -5, pos: { lat: 43.6532, log: -79.3832 } },
+				{ name: 'Vancouver', offset: -8, pos: { lat: 49.2827, log: -123.1207 } },
+				{ name: 'Edmonton', offset: -7, pos: { lat: 53.5461, log: -113.4938 } },
+				{ name: 'Winnipeg', offset: -6, pos: { lat: 49.8951, log: -97.1384 } },
+				{ name: 'Yellowknife', offset: -7, pos: { lat: 62.4540, log: -114.3718 } },
+				{ name: 'Iqaluit', offset: -5, pos: { lat: 63.7467, log: -68.5170 } }
+			],
+			priority: 1,
+			phoneCode: 2
 		},
 		{
 			name: 'Brasil',
-			region: { name: 'South America', code: 'SA' }
+			region: { name: 'South America', code: 'SA' },
+			languages: [{ code: 'PT', name: 'Portuguese' }],
+			timezones: [
+				{ name: 'Sao Paulo', offset: -3, pos: { lat: -23.5505, log: -46.6333 } },
+				{ name: 'Rio de Janeiro', offset: -3, pos: { lat: -22.9068, log: -43.1729 } },
+				{ name: 'Manaus', offset: -4, pos: { lat: -3.1190, log: -60.0217 } },
+				{ name: 'Porto Alegre', offset: -3, pos: { lat: -30.0346, log: -51.2177 } },
+				{ name: 'Fortaleza', offset: -3, pos: { lat: -3.7319, log: -38.5267 } },
+				{ name: 'Belem', offset: -3, pos: { lat: -1.4558, log: -48.4902 } }
+			],
+			priority: 1,
+			phoneCode: 55
 		},
 		{
 			name: 'Argentina',
-			region: { name: 'South America', code: 'SA' }
+			region: { name: 'South America', code: 'SA' },
+			languages: [{ code: 'SP', name: 'Spanish' }],
+			timezones: [{ name: 'Buenos Aires', offset: -3, pos: { lat: -34.6037, log: -58.3816 } }],
+			priority: 1,
+			phoneCode: 54
 		}
 	]
 	const schemaFacade = new SchemaFacadeBuilder(expressions, h3lp).build()
-	const type = Type.type(dataTemp, { info: true })
+	const type = Type.type(data, { info: true })
+	const stringified = Type.stringify(type)
+	console.log(stringified)
 	const schema = schemaFacade.create([{ name: 'Country', type }])
-	console.log(JSON.stringify(schema, null, 2))
+	console.log(JSON.stringify(schema.domain.entities, null, 2))
 })()
--- src/lib/schema/application/services/schemaService.ts
+++ src/lib/schema/application/services/schemaService.ts
@@ -62,22 +62,25 @@ export class SchemaService {
 
 	private objTypeToEntities (name:string, objType:ObjType): Entity[] {
 		const entities:Entity[] = []
-		const key = this.getKey(objType)
-		const primaryKey = key ? [key.name] : []
+		const pk = this.getPk(objType)
+		const primaryKey = pk ? [pk.name] : []
 		const entity:Entity = { name, primaryKey, properties: [], uniqueKey: [], required: [], indexes: [], relations: [], dependents: [] }
 		for (const prop of objType.properties) {
 			if (prop.type && Type.isPrimitive(prop.type)) {
 				const property: Property = { name: prop.name, type: prop.type.primitive, required: (prop.type.nullable === false && prop.type.undefinable === false) }
 				entity.properties.push(property)
+				if (prop.type.unique && (pk === undefined || pk.name !== prop.name)) {
+					entity.uniqueKey.push(prop.name)
+				}
 			} else if (prop.type && Type.isObj(prop.type) && prop.type.obj) {
-				const fk = this.getKey(prop.type.obj)
+				const fk = this.getFk(prop.type.obj)
 				if (fk && fk.type) {
-					const propertyName = prop.name + fk.name
+					const propertyName = prop.name + this.helper.str.capitalize(fk.name)
 					const entityName = this.helper.str.capitalize(prop.name)
 					const property: Property = { name: propertyName, type: fk.type.primitive, required: (prop.type.nullable === false && prop.type.undefinable === false) }
 					const relation:Relation = {
 						name: prop.name,
-						type: RelationType.oneToOne,
+						type: RelationType.oneToMany,
 						from: propertyName,
 						entity: entityName,
 						to: fk.name
@@ -85,26 +88,66 @@ export class SchemaService {
 					entity.properties.push(property)
 					entity.relations.push(relation)
 					const relatedEntities = this.typeToEntities(entityName, prop.type)
+					const relatedEntity = relatedEntities.find(p => p.name === entityName)
+					if (relatedEntity && relatedEntity.primaryKey.length === 0) {
+						relatedEntity.primaryKey = [fk.name]
+					}
 					if (relatedEntities && relatedEntities.length > 0) {
 						entities.push(...relatedEntities)
 					}
 				}
 			} else if (prop.type && Type.isList(prop.type) && prop.type.list?.items.obj) {
-				const fk = this.getKey(prop.type.list.items.obj)
-				if (fk && fk.type) {
-					const propertyName = prop.name + fk.name
+				if (pk && pk.type) {
 					const entityName = this.helper.str.capitalize(prop.name)
-					const property: Property = { name: propertyName, type: fk.type.primitive, required: (prop.type.nullable === false && prop.type.undefinable === false) }
-					const relation:Relation = {
-						name: prop.name,
-						type: RelationType.manyToOne,
-						from: propertyName,
-						entity: entityName,
-						to: fk.name
-					}
-					entity.properties.push(property)
-					entity.relations.push(relation)
 					const relatedEntities = this.typeToEntities(entityName, prop.type)
+					const relatedEntity = relatedEntities.find(p => p.name === entityName)
+					if (relatedEntity) {
+						if (prop.type && prop.type.repeatRate && prop.type.repeatRate > 0.02) {
+							const rpk = this.getFk(prop.type.list?.items.obj)
+							if (rpk && rpk.type) {
+								// hay que crear una tabla intermediaria con el id de la entidad y el id de la entidad relacionada
+								const parentPropertyName = this.helper.str.notation(entity.name, 'camel') + this.helper.str.capitalize(pk.name)
+								const parentProperty: Property = { name: parentPropertyName, type: pk.type.primitive, required: true }
+								const childPropertyName = this.helper.str.singular(this.helper.str.notation(relatedEntity.name, 'camel')) + this.helper.str.capitalize(rpk.name)
+								const childProperty: Property = { name: childPropertyName, type: rpk.type.primitive, required: true }
+								const intermediaEntityName = entity.name + this.helper.str.capitalize(prop.name)
+								const intermediaPropertyPk: Property = { name: 'id', type: 'integer', required: true, autoIncrement: true }
+								const intermediaEntity:Entity = { name: intermediaEntityName, primaryKey: ['id'], properties: [intermediaPropertyPk, childProperty, parentProperty], uniqueKey: [parentProperty.name, childProperty.name], required: [], indexes: [], relations: [], dependents: [] }
+								const parentRelation:Relation = {
+									name: this.helper.str.notation(entity.name, 'camel'),
+									type: RelationType.oneToMany,
+									from: parentPropertyName,
+									entity: entity.name,
+									to: pk.name,
+									target: this.helper.str.notation(intermediaEntityName, 'camel')
+								}
+								const childRelation:Relation = {
+									name: this.helper.str.notation(relatedEntity.name, 'camel'),
+									type: RelationType.oneToMany,
+									from: childPropertyName,
+									entity: relatedEntity.name,
+									to: rpk.name,
+									target: this.helper.str.notation(intermediaEntityName, 'camel')
+								}
+								intermediaEntity.relations.push(parentRelation)
+								intermediaEntity.relations.push(childRelation)
+								entities.push(intermediaEntity)
+							}
+						} else {
+							const propertyName = this.helper.str.notation(entity.name, 'camel') + this.helper.str.capitalize(pk.name)
+							const relatedProperty: Property = { name: propertyName, type: pk.type.primitive, required: true }
+							const relation:Relation = {
+								name: this.helper.str.notation(entity.name, 'camel'),
+								type: RelationType.oneToMany,
+								from: propertyName,
+								entity: entity.name,
+								to: pk.name,
+								target: prop.name
+							}
+							relatedEntity.properties.push(relatedProperty)
+							relatedEntity.relations.push(relation)
+						}
+					}
 					if (relatedEntities && relatedEntities.length > 0) {
 						entities.push(...relatedEntities)
 					}
@@ -115,14 +158,26 @@ export class SchemaService {
 		return entities
 	}
 
-	private getKey (objType:ObjType): PropertyType | undefined {
+	private getPk (objType:ObjType): PropertyType | undefined {
 		const uniques = objType.properties.filter(p => p.type?.unique === true)
+		return this.getKey(uniques)
+	}
+
+	private getFk (objType:ObjType): PropertyType | undefined {
+		const uniques = objType.properties.filter(p => p.type?.onParentDistinctUnique === true)
+		const result = this.getKey(uniques)
+		return result
+	}
+
+	private getKey (uniques:PropertyType[]): PropertyType | undefined {
 		if (uniques.length === 1) {
 			return uniques[0]
 		} else if (uniques.length > 1) {
-			const id = uniques.find(p => ['id', 'name', 'code', 'key'].includes(p.name.toLowerCase()))
-			if (id) {
-				return id
+			for (const keyName of ['id', 'code', 'key', 'name']) {
+				const id = uniques.find(p => p.name.toLowerCase() === keyName)
+				if (id) {
+					return id
+				}
 			}
 			const idNumber = uniques.find(p => p.type?.primitive === 'number' || p.type?.primitive === 'integer')
 			if (idNumber) {
